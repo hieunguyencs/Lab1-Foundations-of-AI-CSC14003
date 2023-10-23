21ecdc08ae3d167553604478117cd12d2c5d4851
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2023-10-23 18:44:56 +0700

    update bfs_visualizer

--- src/bfs_visualizer.py
+++ src/bfs_visualizer.py
@@ -4,24 +4,21 @@ import sys
 from collections import deque
 
 # GAME SETUP
-WIDTH, HEIGHT = 1500, 800
+WIDTH, HEIGHT = 1200, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("BFS")
 FPS = 60
 delay = 30
 
-
 # DEFINE COLOR
 WHITE = (255, 255, 255)
 
-
-# Constants
+# CONSTANTS
 CELL_WIDTH, CELL_HEIGHT = 50, 50
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 ROW, COL = 0, 0
 X_OFFSET, Y_OFFSET = 0, 0
-
 
 # INCLUDE IMAGE
 START_IMG = pygame.image.load(os.path.join('..', 'Assets', 'start.jpg'))
@@ -39,6 +36,8 @@ WALL_IMG = pygame.transform.scale(WALL_IMG, (CELL_WIDTH, CELL_HEIGHT))
 VISITED_IMG = pygame.transform.scale(VISITED_IMG, (CELL_WIDTH, CELL_HEIGHT))
 PATH_IMG = pygame.transform.scale(PATH_IMG, (CELL_WIDTH, CELL_HEIGHT))
 
+
+# DRAW METHOD
 def draw_cell_no_delay(x, y, IMG): 
     drawX = X_OFFSET + y * CELL_WIDTH
     drawY = Y_OFFSET + x * CELL_HEIGHT
@@ -51,25 +50,6 @@ def draw_cell(x, y, IMG):
     pygame.display.update()
     pygame.time.delay(delay)
 
-def load_maze(maze_path):
-    maze_data = []
-    gift_data = []
-    with open(maze_path, 'r') as file:
-        lines = file.read().splitlines()
-        # The first line is the number of rows (ignore it)
-
-        n = int(list(lines[0])[0])
-        for i in range(1, n + 1): 
-            gift_data.append(list(lines[i]))
-
-        for line in lines[n + 1:]:
-            maze_data.append(list(line))
-
-        rows = len(lines) - n - 1
-        cols = len(lines[n + 1])
-
-    return maze_data, rows, cols
-
 def draw_maze(maze_data, rows, cols):
     for row in range(rows):
         for col in range(cols):
@@ -80,6 +60,54 @@ def draw_maze(maze_data, rows, cols):
                 draw_cell_no_delay(row, col, START_IMG)
             elif row == 0 or row == rows - 1 or col == 0 or col == cols - 1:
                 draw_cell_no_delay(row, col, END_IMG)
+            elif cell == '+': 
+                draw_cell_no_delay(row, col, GIFT_IMG)
+
+# LOAD MAZE GIVEN PATH
+def load_maze(maze_path):
+    # Set caption
+    map_number = int(maze_path.split('/')[-1].replace('input', '').split('.')[0])
+    cur_caption = pygame.display.get_caption()[0]
+    new_caption = f"{cur_caption} - Map {map_number}"
+    pygame.display.set_caption(new_caption)
+
+    # Read maze
+    maze_data = []
+    gift_data = []
+    with open(maze_path, 'r') as file:
+        lines = file.read().splitlines()
+
+        n = list(map(int, lines[0].split()))[0]
+        for i in range(1, n + 1): 
+            gift_data.append(list(map(int, lines[i].split())))
+
+        for line in lines[n + 1:]:
+            maze_data.append(list(line))
+
+        rows = len(lines) - n - 1
+        cols = len(lines[n + 1])
+
+    # Set up sizes and position
+    global CELL_WIDTH, CELL_HEIGHT
+    if WIDTH / cols < HEIGHT / rows: 
+        CELL_WIDTH = CELL_HEIGHT = (WIDTH) / (cols + 2)
+    else: 
+        CELL_WIDTH = CELL_HEIGHT = (HEIGHT) / (rows + 2)
+
+    global X_OFFSET, Y_OFFSET   
+    X_OFFSET = (WIDTH - cols * CELL_WIDTH) // 2
+    Y_OFFSET = (HEIGHT - rows * CELL_HEIGHT) // 2
+
+    # Draw maze
+    WIN.fill(WHITE)
+    draw_maze(maze_data, rows, cols)
+    pygame.display.update()
+    pygame.time.delay(1000)
+
+    return maze_data, gift_data, rows, cols
+
+# --- WRITE GRAPH FUNCTION HERE ---
+# You can call function draw_cell(x, y, IMG) to draw IMG at cell (x, y)
 
 def bfs(grid, rows, cols): 
     start_x, start_y = None, None
@@ -137,19 +165,16 @@ def draw_path(grid, rows, cols):
     for x, y in path:
         draw_cell(x, y, PATH_IMG)
 
+# ---------------------------------
+
 def main(maze_path):
-    maze_data, rows, cols = load_maze(maze_path)
-    
-    global X_OFFSET, Y_OFFSET   
-    X_OFFSET = (WIDTH - cols * CELL_WIDTH) // 2
-    Y_OFFSET = (HEIGHT - rows * CELL_HEIGHT) // 2
+    maze_data, gift_data, rows, cols = load_maze(maze_path)
 
-    WIN.fill(WHITE)
-    draw_maze(maze_data, rows, cols)
-    pygame.display.update()
-    pygame.time.delay(1000)
-
+    # --- CALL GRAPH FUNCTION HERE ---
+    # Ex: DFS(maze_data, gift_data, rows, cols)
     draw_path(maze_data, rows, cols)
+
+    # --------------------------------
 
     clock = pygame.time.Clock()
     run = True
@@ -159,7 +184,6 @@ def main(maze_path):
             if event.type == pygame.QUIT:
                 run = False
     pygame.quit()
-
 
 if len(sys.argv) != 2:
     print("Usage: python bfs_visualizer.py <path>")
