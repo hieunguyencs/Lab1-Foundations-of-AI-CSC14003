f1f47e2eb85fef80f11e22fc59ce1c96344b5a8e
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2023-10-23 14:47:54 +0700

    add pygame_template.py @ src

---
+++ src/pygame_template.py
@@ -0,0 +1,123 @@
+import pygame
+import os
+import sys
+from collections import deque
+
+# GAME SETUP
+WIDTH, HEIGHT = 1500, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("BFS")
+FPS = 60
+delay = 30
+
+
+# DEFINE COLOR
+WHITE = (255, 255, 255)
+
+
+# Constants
+CELL_WIDTH, CELL_HEIGHT = 50, 50
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+ROW, COL = 0, 0
+X_OFFSET, Y_OFFSET = 0, 0
+
+
+# INCLUDE IMAGE
+START_IMG = pygame.image.load(os.path.join('..', 'Assets', 'start.jpg'))
+END_IMG = pygame.image.load(os.path.join('..', 'Assets', 'door.jpg'))
+GIFT_IMG = pygame.image.load(os.path.join('..', 'Assets', 'gift.jpg'))
+WALL_IMG = pygame.image.load(os.path.join('..', 'Assets', 'wall.jpg'))
+VISITED_IMG = pygame.image.load(os.path.join('..', 'Assets', 'visited.jpg'))
+PATH_IMG = pygame.image.load(os.path.join('..', 'Assets', 'path.jpg'))
+
+# SCALE IMG
+START_IMG = pygame.transform.scale(START_IMG, (CELL_WIDTH, CELL_HEIGHT))
+END_IMG = pygame.transform.scale(END_IMG, (CELL_WIDTH, CELL_HEIGHT))
+GIFT_IMG = pygame.transform.scale(GIFT_IMG, (CELL_WIDTH, CELL_HEIGHT))
+WALL_IMG = pygame.transform.scale(WALL_IMG, (CELL_WIDTH, CELL_HEIGHT))
+VISITED_IMG = pygame.transform.scale(VISITED_IMG, (CELL_WIDTH, CELL_HEIGHT))
+PATH_IMG = pygame.transform.scale(PATH_IMG, (CELL_WIDTH, CELL_HEIGHT))
+
+def draw_cell_no_delay(x, y, IMG): 
+    drawX = X_OFFSET + y * CELL_WIDTH
+    drawY = Y_OFFSET + x * CELL_HEIGHT
+    WIN.blit(IMG, (drawX, drawY))
+
+def draw_cell(x, y, IMG): 
+    drawX = X_OFFSET + y * CELL_WIDTH
+    drawY = Y_OFFSET + x * CELL_HEIGHT
+    WIN.blit(IMG, (drawX, drawY))
+    pygame.display.update()
+    pygame.time.delay(delay)
+
+def load_maze(maze_path):
+    maze_data = []
+    gift_data = []
+    with open(maze_path, 'r') as file:
+        lines = file.read().splitlines()
+        # The first line is the number of rows (ignore it)
+
+        n = int(list(lines[0])[0])
+        for i in range(1, n + 1): 
+            gift_data.append(list(lines[i]))
+
+        for line in lines[n + 1:]:
+            maze_data.append(list(line))
+
+        rows = len(lines) - n - 1
+        cols = len(lines[n + 1])
+
+    return maze_data, gift_data, rows, cols
+
+def draw_maze(maze_data, rows, cols):
+    for row in range(rows):
+        for col in range(cols):
+            cell = maze_data[row][col]
+            if cell == 'x':
+                draw_cell_no_delay(row, col, WALL_IMG)
+            elif cell == 'S':
+                draw_cell_no_delay(row, col, START_IMG)
+            elif row == 0 or row == rows - 1 or col == 0 or col == cols - 1:
+                draw_cell_no_delay(row, col, END_IMG)
+            elif cell == '+': 
+                draw_cell_no_delay(row, col, GIFT_IMG)
+
+# --- WRITE GRAPH FUNCTION HERE ---
+
+
+
+# ---------------------------------
+
+def main(maze_path):
+    maze_data, gift_data, rows, cols = load_maze(maze_path)
+    
+    global X_OFFSET, Y_OFFSET   
+    X_OFFSET = (WIDTH - cols * CELL_WIDTH) // 2
+    Y_OFFSET = (HEIGHT - rows * CELL_HEIGHT) // 2
+
+    WIN.fill(WHITE)
+    draw_maze(maze_data, rows, cols)
+    pygame.display.update()
+    pygame.time.delay(1000)
+
+    # --- CALL GRAPH FUNCTION HERE ---
+
+
+    # --------------------------------
+
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+
+if len(sys.argv) != 2:
+    print("Usage: python bfs_visualizer.py <path>")
+else:
+    maze_path = sys.argv[1]
+    main(maze_path)
