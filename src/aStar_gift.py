58847c4081a37b76dd0b36685e350fa7b308b86d
Hieu T. Nguyen <nguyentronghieu@hsgs.edu.vn>
2023-10-29 15:12:08 +0700

    add argo  (greedy) to solve gift prob

---
+++ src/aStar_gift.py
@@ -0,0 +1,245 @@
+import pygame
+import os
+import sys
+import heapq
+import math
+
+# GAME SETUP
+WIDTH, HEIGHT = 1200, 700
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("A*")
+FPS = 60
+delay = 30
+long_delay = 1000
+
+# DEFINE COLOR
+WHITE = (255, 255, 255)
+
+# CONSTANTS
+CELL_WIDTH, CELL_HEIGHT = 50, 50
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+ROW, COL = 0, 0
+X_OFFSET, Y_OFFSET = 0, 0
+
+# INCLUDE IMAGE
+START_IMG = pygame.image.load(os.path.join('Assets', 'start.jpg'))
+END_IMG = pygame.image.load(os.path.join('Assets', 'door.jpg'))
+GIFT_IMG = pygame.image.load(os.path.join('Assets', 'gift.jpg'))
+WALL_IMG = pygame.image.load(os.path.join('Assets', 'wall.jpg'))
+VISITED_IMG = pygame.image.load(os.path.join('Assets', 'visited.jpg'))
+PATH_IMG = pygame.image.load(os.path.join('Assets', 'path.jpg'))
+
+
+# SCALE IMAGE
+def scale_img():
+    global START_IMG, END_IMG, GIFT_IMG, WALL_IMG, VISITED_IMG, PATH_IMG
+    START_IMG = pygame.transform.scale(START_IMG, (CELL_WIDTH, CELL_HEIGHT))
+    END_IMG = pygame.transform.scale(END_IMG, (CELL_WIDTH, CELL_HEIGHT))
+    GIFT_IMG = pygame.transform.scale(GIFT_IMG, (CELL_WIDTH, CELL_HEIGHT))
+    WALL_IMG = pygame.transform.scale(WALL_IMG, (CELL_WIDTH, CELL_HEIGHT))
+    VISITED_IMG = pygame.transform.scale(VISITED_IMG, (CELL_WIDTH, CELL_HEIGHT))
+    PATH_IMG = pygame.transform.scale(PATH_IMG, (CELL_WIDTH, CELL_HEIGHT))
+
+
+# DRAW METHOD
+def draw_cell_no_delay(x, y, IMG):
+    drawX = X_OFFSET + y * CELL_WIDTH
+    drawY = Y_OFFSET + x * CELL_HEIGHT
+    WIN.blit(IMG, (drawX, drawY))
+
+
+def draw_cell(x, y, IMG):
+    drawX = X_OFFSET + y * CELL_WIDTH
+    drawY = Y_OFFSET + x * CELL_HEIGHT
+    WIN.blit(IMG, (drawX, drawY))
+    pygame.display.update()
+    pygame.time.delay(delay)
+
+
+def draw_maze(maze_data, rows, cols):
+    for row in range(rows):
+        for col in range(cols):
+            cell = maze_data[row][col]
+            if cell == 'x':
+                draw_cell_no_delay(row, col, WALL_IMG)
+            elif cell == 'S':
+                draw_cell_no_delay(row, col, START_IMG)
+            elif row == 0 or row == rows - 1 or col == 0 or col == cols - 1:
+                draw_cell_no_delay(row, col, END_IMG)
+            elif cell == '+':
+                draw_cell_no_delay(row, col, GIFT_IMG)
+
+
+# LOAD MAZE GIVEN PATH
+def load_maze(maze_path):
+    # Set caption
+    map_number = int(maze_path.split('/')[-1].replace('input', '').split('.')[0])
+    cur_caption = pygame.display.get_caption()[0]
+    new_caption = f"{cur_caption} - Map {map_number}"
+    pygame.display.set_caption(new_caption)
+
+    # Read maze
+    maze_data = []
+    gift_data = []
+    with open(maze_path, 'r') as file:
+        lines = file.read().splitlines()
+
+        n = list(map(int, lines[0].split()))[0]
+        for i in range(1, n + 1):
+            gift_data.append(list(map(int, lines[i].split())))
+
+        for line in lines[n + 1:]:
+            maze_data.append(list(line))
+
+        rows = len(lines) - n - 1
+        cols = len(lines[n + 1])
+
+    # Set up sizes and position
+    global CELL_WIDTH, CELL_HEIGHT
+    if WIDTH / cols < HEIGHT / rows:
+        CELL_WIDTH = CELL_HEIGHT = (WIDTH) / (cols + 2)
+    else:
+        CELL_WIDTH = CELL_HEIGHT = (HEIGHT) / (rows + 2)
+
+    global X_OFFSET, Y_OFFSET
+    X_OFFSET = (WIDTH - cols * CELL_WIDTH) // 2
+    Y_OFFSET = (HEIGHT - rows * CELL_HEIGHT) // 2
+
+    scale_img()
+
+    # Draw maze
+    WIN.fill(WHITE)
+    draw_maze(maze_data, rows, cols)
+    pygame.display.update()
+    pygame.time.delay(long_delay)
+
+    return maze_data, gift_data, rows, cols
+
+
+# --- WRITE GRAPH FUNCTION HERE ---
+# You can call function draw_cell(x, y, IMG) to draw IMG at cell (x, y)
+
+def find_start(grid, num_row, num_col):
+    for i in range(num_row):
+        for j in range(num_col):
+            if grid[i][j] == 'S':
+                return [i, j]
+
+
+def find_end(grid, num_row, num_col):
+    for i in range(num_row):
+        for j in range(num_col):
+            if (grid[i][j] != 'x'
+                    and (i == 0 or i == num_row - 1
+                         or j == 0 or j == num_col - 1)):
+                return [i, j]
+
+def aStarForGiftProb(grid, num_row, num_col, start, end):
+    MAX_DIS = 1_000_000_000
+    distance = [[MAX_DIS for i in range(num_col)] for i in range(num_row)]
+    trace = [[[-1, -1] for i in range(num_col)] for i in range(num_row)]
+    open = [[0 for i in range(num_col)] for i in range(num_row)]
+    heap = []  # this is for getting min f = g + h
+
+    # heuristic: manhattan distance
+    def h(x, y):
+        X = abs(x - end[0])
+        Y = abs(y - end[1])
+        return X + Y
+
+    # trace path
+    def constructPath():
+        X, Y = end
+        path = []
+        while [X, Y] != [-1, -1]:
+            path.append([X, Y])
+            X, Y = trace[X][Y]
+        path.reverse()
+        return path
+
+    def inGrid(x, y):
+        return (x >= 0 and x < num_row
+                and y >= 0 and y < num_col)
+
+    distance[start[0]][start[1]] = 0
+    heapq.heappush(heap, [h(start[0], start[1]), start])
+    open[start[0]][start[1]] = True
+
+    while len(heap) > 0:
+        cur = heapq.heappop(heap)
+        cur_cell = cur[1]
+
+        if (cur_cell == end):
+            return constructPath()
+
+        if cur_cell != start:
+            draw_cell(cur_cell[0], cur_cell[1], VISITED_IMG)
+
+        open[cur_cell[0]][cur_cell[1]] = False
+        for i in range(4):
+            newX = cur_cell[0] + dx[i]
+            newY = cur_cell[1] + dy[i]
+
+            if not inGrid(newX, newY) or grid[newX][newY] == 'x':
+                continue
+
+            proposal_gScore = distance[cur_cell[0]][cur_cell[1]] + 1;
+            if (proposal_gScore < distance[newX][newY]):
+                distance[newX][newY] = proposal_gScore
+                fScore = proposal_gScore + h(newX, newY)
+                trace[newX][newY] = cur_cell
+                if not open[newX][newY]:
+                    heapq.heappush(heap, [fScore, [newX, newY]])
+
+    return []
+
+
+def draw_path(path):
+    pygame.time.delay(long_delay)
+    path = path[1:-1]
+    for x, y in path:
+        draw_cell(x, y, PATH_IMG)
+
+def remove_path(path):
+    pygame.time.delay(long_delay)
+    path = path[1:-1]
+    for x, y in path:
+        draw_cell(x, y, VISITED_IMG)
+
+
+# ---------------------------------
+
+def main(maze_path):
+    maze_data, gift_data, rows, cols = load_maze(maze_path)
+
+    # --- CALL GRAPH FUNCTION HERE ---
+    # Ex: DFS(maze_data, gift_data, rows, cols)
+    gifts_sort = sorted(gift_data, key=lambda point: point[2])
+    start = find_start(maze_data, rows, cols)
+    end = find_end(maze_data, rows, cols)
+    for i in range(len(gifts_sort)):
+        gift = [gifts_sort[i][0], gifts_sort[i][1]]
+        path = aStarForGiftProb(maze_data, rows, cols, start, gift)
+        draw_path(path)
+        start = gift
+    path = aStarForGiftProb(maze_data, rows, cols, start, end)
+    draw_path(path)
+
+    # --------------------------------
+
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+
+if len(sys.argv) != 2:
+    print("Usage: python bfs_visualizer.py <path>")
+else:
+    maze_path = sys.argv[1]
+    main(maze_path)
