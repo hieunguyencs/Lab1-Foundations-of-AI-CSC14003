5f39b450e68c4fbef4cfc963676c533c07a43559
Hieu T. Nguyen <nguyentronghieu@hsgs.edu.vn>
2023-10-30 20:54:10 +0700

    add func to check duplicate point in path

--- src/WriteOutput.py
+++ src/WriteOutput.py
@@ -1,12 +1,21 @@
 def writeToFile(file_name="output.txt", path=None):
-    #Check if the path contains duplicated point?
     if path==None:
         return
-    print(path)
+    print("Check path again:\n", path)
     with open(file_name, 'w') as out:
         out.write(str(len(path)))
-        out.write('\n')
-        for point in path:
-            out.write('({}, {}) '.format(point[0], point[1]))
+        #write path
+        #out.write('\n')
+        #for point in path:
+        #    out.write('({}, {}) '.format(point[0], point[1]))
 
+
+def checkDuplicatePointInPath(path):
+    s = set()
+    for point in path:
+        p_tuple = tuple(point)
+        if p_tuple in s:
+            return True
+        s.add(p_tuple)
+    return False
 
