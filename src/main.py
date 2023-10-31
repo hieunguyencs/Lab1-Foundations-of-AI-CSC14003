06e49f41a82a489e44a5b545477413934b7aa8f3
Hieu T. Nguyen <nguyentronghieu@hsgs.edu.vn>
2023-10-31 08:01:45 +0700

    delete cmd /c in main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ import os
 
 INPUT_ROOT = "../input/"
 def main():
-    os.system(f"cmd /c python bfs_visualizer.py ../input/level_1/input1.txt")
-    os.system(f"cmd /c python dfs_visualizer.py ../input/level_1/input1.txt")
     solve(INPUT_ROOT + "level_1/")
     solve(INPUT_ROOT + "level_2/")
     solve(INPUT_ROOT + "level_3/")
@@ -14,20 +12,20 @@ def solve(prob_path):
         file_path = os.path.join(prob_path, f)
         if os.path.isfile(file_path):
             if "level_1" in prob_path:
-                os.system(f"cmd /c python bfs_visualizer.py {file_path}")
-                os.system(f"cmd /c python dfs_visualizer.py {file_path}")
-                os.system(f"cmd /c python aStar_heuristic1_visualizer.py {file_path}")
-                os.system(f"cmd /c python aStar_heuristic2_visualizer.py {file_path}")
-                os.system(f"cmd /c python GBFS_heuristic1_visualizer.py {file_path}")
-                os.system(f"cmd /c python GBFS_heuristic2_visualizer.py {file_path}")
-                os.system(f"cmd /c python ucs_visualizer.py {file_path}")
+                os.system(f"python bfs_visualizer.py {file_path}")
+                os.system(f"python dfs_visualizer.py {file_path}")
+                os.system(f"python aStar_heuristic1_visualizer.py {file_path}")
+                os.system(f"python aStar_heuristic2_visualizer.py {file_path}")
+                os.system(f"python GBFS_heuristic1_visualizer.py {file_path}")
+                os.system(f"python GBFS_heuristic2_visualizer.py {file_path}")
+                os.system(f"python ucs_visualizer.py {file_path}")
 
             elif "level_2" in prob_path:
-                os.system(f"cmd /c python gift_aStar.py {file_path}")
+                os.system(f"python gift_aStar.py {file_path}")
             elif "level_3" in prob_path:
-                os.system(f"cmd /c python GBFS_station.py {file_path}")
+                os.system(f"python GBFS_station.py {file_path}")
             elif "advance" in prob_path:
-                os.system(f"cmd /c python teleport_visualizer.py {file_path}")
+                os.system(f"python teleport_visualizer.py {file_path}")
 
 if __name__ == "__main__":
     main()
